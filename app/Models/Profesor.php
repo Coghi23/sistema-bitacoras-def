4da879d03796cc8a18ef49639d886e402f388c42
sarilu15 <sara.sarmiento.gutierrez@covao.ed.cr>
2025-07-14 15:22:06 -0600

    modelos base

---
+++ app/Models/Profesor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profesor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'profesor';
+    public $timestamps = true;
+
+    protected $fillable = ['usuario_id'];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function especialidades()
+    {
+        return $this->belongstoMany(Especialidad::class);
+    }
+
+    public function horarios()
+    {
+        return $this->belongsToMany(Horario::class);
+    }
+    public function eventos()
+    {
+        return $this->belongsToMany(Evento::class);
+    }
+    
+}
