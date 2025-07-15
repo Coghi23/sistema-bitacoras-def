0cddaf3faa96b113e0dc0654219f1a3971323418
sarilu15 <sara.sarmiento.gutierrez@covao.ed.cr>
2025-07-14 20:23:15 -0600

    recinto cambio

---
+++ app/Models/Recinto.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recinto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'recinto';   
+    public $timestamps = false;     
+
+    protected $fillable = [
+        'institucion_id',
+        'nombre',
+        'tipo',
+        'estado',
+        'condicion',
+    ];
+
+    // Un recinto *pertenece a* una institución via institucion_id
+    public function institucion()
+    {
+        return $this->belongsTo(Institucion::class);
+    }
+
+    // Un recinto *puede tener* múltiples horarios
+    public function horarios()
+    {
+        return $this->hasMany(Horario::class);
+    }
+}
