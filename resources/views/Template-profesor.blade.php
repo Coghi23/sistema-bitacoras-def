377cdefd1353dab524a94498b58312d0b6e445b6
Coghi23 <134116795+Coghi23@users.noreply.github.com>
2025-08-12 13:00:31 -0600

    recxinto

--- resources/views/Template-profesor.blade.php
+++ resources/views/Template-profesor.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('Css/Sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('Css/indexBitacoras.css') }}">
     @stack('styles')
 </head>
 
@@ -58,6 +59,7 @@
     @yield('content')
 
     <script src="{{ asset('JS/Sidebar.js') }}"></script>
+    <script src="{{ asset('JS/indexBitacoras.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     @stack('scripts')
 </body>
